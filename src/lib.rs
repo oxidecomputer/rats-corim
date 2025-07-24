a42962e400bc51633ae685146069423562ef04f2
Laura Abbott <laura@oxide.computer>
2025-07-24 14:36:47 -0400

    from bytes

--- src/lib.rs
+++ src/lib.rs
@@ -921,6 +921,11 @@ impl Corim {
         }
     }
 
+    pub fn from_bytes(bytes: &[u8]) -> Result<Self, Error> {
+        ciborium::from_reader(&bytes[..])
+            .map_err(|e| Error::Deserialize(format!("from bytes {:?}", e)))
+    }
+
     pub fn from_file<P: AsRef<std::path::Path>>(path: P) -> Result<Self, Error> {
         let bytes = std::fs::read(&path).map_err(Error::Io)?;
         ciborium::from_reader(&bytes[..])
